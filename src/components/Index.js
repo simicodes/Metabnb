7b3e6f7409fe643ba01f9e315c627a8295e51018
simicodes <simeonoffiong@gmail.com>
2022-11-15 12:02:42 +0100

    Metabnb project

---
+++ src/components/Index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Footer from './Footer';
+import Header from './Header';
+import Hero from './Hero';
+import Meta from './Meta';
+import Modal from './Modal';
+import Place from './Place';
+import SocialProof from './SocialProof';
+
+const Index = () => {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <SocialProof />
+      <Place />
+      <Meta />
+      <Footer />
+    </>
+  );
+};
+
+export default Index;
